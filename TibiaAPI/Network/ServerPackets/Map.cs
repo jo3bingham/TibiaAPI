a233748d0211167e18b52cc30f0fc0bf7e19c959
Joseph Bingham <jo3bingham@gmail.com>
2018-10-10 17:47:32 +0100

    Add missing Map base class file

---
+++ TibiaAPI/Network/ServerPackets/Map.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+using OXGaming.TibiaAPI.Utilities;
+using OXGaming.TibiaAPI.WorldMap;
+
+namespace OXGaming.TibiaAPI.Network.ServerPackets
+{
+    public class Map : ServerPacket
+    {
+        public List<(Field, Position)> Fields { get; } = new List<(Field, Position)>();
+    }
+}
