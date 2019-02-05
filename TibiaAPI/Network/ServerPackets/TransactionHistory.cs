47e8d24594776404412606ef0d30f0c5e4f3035b
jo3bingham <jo3bingham@gmail.com>
2019-02-05 17:29:18 +0000

    Add changes to TransactionHistory server packet

--- TibiaAPI/Network/ServerPackets/TransactionHistory.cs
+++ TibiaAPI/Network/ServerPackets/TransactionHistory.cs
@@ -7,6 +7,8 @@ namespace OXGaming.TibiaAPI.Network.ServerPackets
 {
     public class TransactionHistory : ServerPacket
     {
+        public List<(uint Id, uint Timestamp, byte Unknown1, int CreditChange, byte Unknown2, string Description, bool HasDetails)> NewTransactions { get; } =
+            new List<(uint Id, uint Timestamp, byte Unknown1, int CreditChange, byte Unknown2, string Description, bool HasDetails)>();
         public List<(uint Timestamp, byte Type, int CreditChange, string Name)> Transactions { get; } =
             new List<(uint Timestamp, byte Type, int CreditChange, string Name)>();
 
@@ -28,14 +30,38 @@ namespace OXGaming.TibiaAPI.Network.ServerPackets
 
             CurrentPage = message.ReadUInt32();
             NumberOfPages = message.ReadUInt32();
-            Transactions.Capacity = message.ReadByte();
-            for (var i = 0; i < Transactions.Capacity; ++i)
+
+            var count = message.ReadByte();
+            if (Client.VersionNumber >= 12087995)
             {
-                var timestamp = message.ReadUInt32();
-                var type = message.ReadByte();
-                var creditChange = message.ReadInt32();
-                var name = message.ReadString();
-                Transactions.Add((timestamp, type, creditChange, name));
+                NewTransactions.Capacity = count;
+            }
+            else
+            {
+                Transactions.Capacity = count;
+            }
+
+            for (var i = 0; i < count; ++i)
+            {
+                if (Client.VersionNumber >= 12087995)
+                {
+                    var id = message.ReadUInt32();
+                    var timestamp = message.ReadUInt32();
+                    var unknown1 = message.ReadByte();
+                    var creditChange = message.ReadInt32();
+                    var unknown2 = message.ReadByte();
+                    var description = message.ReadString();
+                    var hasDetails = message.ReadBool();
+                    NewTransactions.Add((id, timestamp, unknown1, creditChange, unknown2, description, hasDetails));
+                }
+                else
+                {
+                    var timestamp = message.ReadUInt32();
+                    var type = message.ReadByte();
+                    var creditChange = message.ReadInt32();
+                    var name = message.ReadString();
+                    Transactions.Add((timestamp, type, creditChange, name));
+                }
             }
             return true;
         }
@@ -45,15 +71,29 @@ namespace OXGaming.TibiaAPI.Network.ServerPackets
             message.Write((byte)ServerPacketType.TransactionHistory);
             message.Write(CurrentPage);
             message.Write(NumberOfPages);
-            var count = Math.Min(Transactions.Count, byte.MaxValue);
+            var count = Client.VersionNumber >= 12087995 ? Math.Min(NewTransactions.Count, byte.MaxValue) : Math.Min(Transactions.Count, byte.MaxValue);
             message.Write((byte)count);
             for (var i = 0; i < count; ++i)
             {
-                var transaction = Transactions[i];
-                message.Write(transaction.Timestamp);
-                message.Write(transaction.Type);
-                message.Write(transaction.CreditChange);
-                message.Write(transaction.Name);
+                if (Client.VersionNumber >= 12087995)
+                {
+                    var (Id, Timestamp, Unknown1, CreditChange, Unknown2, Description, HasDetails) = NewTransactions[i];
+                    message.Write(Id);
+                    message.Write(Timestamp);
+                    message.Write(Unknown1);
+                    message.Write(CreditChange);
+                    message.Write(Unknown2);
+                    message.Write(Description);
+                    message.Write(HasDetails);
+                }
+                else
+                {
+                    var transaction = Transactions[i];
+                    message.Write(transaction.Timestamp);
+                    message.Write(transaction.Type);
+                    message.Write(transaction.CreditChange);
+                    message.Write(transaction.Name);
+                }
             }
         }
     }
