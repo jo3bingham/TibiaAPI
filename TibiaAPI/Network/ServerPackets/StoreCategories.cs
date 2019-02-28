c3fe441135dd1e0ea37aa99e7cb878661ed2eadd
jo3bingham <jo3bingham@gmail.com>
2019-02-28 12:18:22 +0000

    Add support for writing StoreCategories server packets to NetworkMessage

--- TibiaAPI/Network/ServerPackets/StoreCategories.cs
+++ TibiaAPI/Network/ServerPackets/StoreCategories.cs
@@ -1,9 +1,15 @@
-﻿using OXGaming.TibiaAPI.Constants;
+﻿using System;
+using System.Collections.Generic;
+
+using OXGaming.TibiaAPI.Constants;
 
 namespace OXGaming.TibiaAPI.Network.ServerPackets
 {
     public class StoreCategories : ServerPacket
     {
+        public List<(string Name, byte HighlightState, List<string> Icons, string ParentName)> Categories { get; } =
+            new List<(string Name, byte HighlightState, List<string> Icons, string ParentName)>();
+
         public StoreCategories(Client client)
         {
             Client = client;
@@ -17,19 +23,21 @@ namespace OXGaming.TibiaAPI.Network.ServerPackets
                 return false;
             }
 
-            var numberOfShopCategories = message.ReadUInt16();
-            for (var i = 0; i < numberOfShopCategories; ++i)
+            Categories.Capacity = message.ReadUInt16();
+            for (var i = 0; i < Categories.Capacity; ++i)
             {
-                var categoryName = message.ReadString();
+                var name = message.ReadString();
                 var highlightState = message.ReadByte();
 
-                var numberOfIconIdentifiers = message.ReadByte();
-                for (var j = 0; j < numberOfIconIdentifiers; ++j)
+                var icons = new List<string>(message.ReadByte());
+                for (var j = 0; j < icons.Capacity; ++j)
                 {
-                    var iconName = message.ReadString();
+                    icons.Add(message.ReadString());
                 }
 
                 var parentName = message.ReadString();
+
+                Categories.Add((name, highlightState, icons, parentName));
             }
             return true;
         }
@@ -37,7 +45,24 @@ namespace OXGaming.TibiaAPI.Network.ServerPackets
         public override void AppendToNetworkMessage(NetworkMessage message)
         {
             message.Write((byte)ServerPacketType.StoreCategories);
-            // TODO
+
+            var count = Math.Min(Categories.Count, ushort.MaxValue);
+            message.Write((ushort)count);
+            for (var i = 0; i < count; ++i)
+            {
+                var (Name, HighlightState, Icons, ParentName) = Categories[i];
+                message.Write(Name);
+                message.Write(HighlightState);
+
+                var size = Math.Min(Icons.Count, byte.MaxValue);
+                message.Write((byte)size);
+                for (var j = 0; j < size; ++j)
+                {
+                    message.Write(Icons[j]);
+                }
+
+                message.Write(ParentName);
+            }
         }
     }
 }
