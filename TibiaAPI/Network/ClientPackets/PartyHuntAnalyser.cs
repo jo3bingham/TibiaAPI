41c3c6467b977ecea3cefb9717e1ddd4bffd606b
Joseph Bingham <jo3bingham@gmail.com>
2020-07-08 15:34:55 -0400

    Add missing packets to be handled

---
+++ TibiaAPI/Network/ClientPackets/PartyHuntAnalyser.cs
@@ -0,0 +1,24 @@
+﻿using OXGaming.TibiaAPI.Constants;
+
+namespace OXGaming.TibiaAPI.Network.ClientPackets
+{
+    public class PartyHuntAnalyser : ClientPacket
+    {
+        public PartyHuntAnalyser(Client client)
+        {
+            Client = client;
+            PacketType = ClientPacketType.PartyHuntAnalyser;
+        }
+
+        public override void ParseFromNetworkMessage(NetworkMessage message)
+        {
+            // TODO
+        }
+
+        public override void AppendToNetworkMessage(NetworkMessage message)
+        {
+            // TODO
+            // message.Write((byte)ClientPacketType.PartyHuntAnalyser);
+        }
+    }
+}
---
+++ TibiaAPI/Network/ServerPackets/CyclopediaHouseActionResult.cs
@@ -0,0 +1,24 @@
+﻿using OXGaming.TibiaAPI.Constants;
+
+namespace OXGaming.TibiaAPI.Network.ServerPackets
+{
+    public class CyclopediaHouseActionResult : ServerPacket
+    {
+        public CyclopediaHouseActionResult(Client client)
+        {
+            Client = client;
+            PacketType = ServerPacketType.CyclopediaHouseActionResult;
+        }
+
+        public override void ParseFromNetworkMessage(NetworkMessage message)
+        {
+            // TODO
+        }
+
+        public override void AppendToNetworkMessage(NetworkMessage message)
+        {
+            // TODO
+            // message.Write((byte)ServerPacketType.CyclopediaHouseActionResult);
+        }
+    }
+}
