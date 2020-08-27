22ea054ad2f8c172279d24daaa41d5eceaf66aa7
Joseph Bingham <jo3bingham@gmail.com>
2020-08-27 11:50:55 -0400

    Add new unknown to CreditBalance

--- TibiaAPI/Network/ServerPackets/CreditBalance.cs
+++ TibiaAPI/Network/ServerPackets/CreditBalance.cs
@@ -7,6 +7,7 @@ namespace OXGaming.TibiaAPI.Network.ServerPackets
         public int TransferableTibiaCoins { get; set; }
         public int TotalTibiaCoins { get; set; }
         public int TournamentCoins { get; set; }
+        public int Unknown { get; set; }
 
         public bool UpdateCreditBalance { get; set; }
 
@@ -23,6 +24,10 @@ namespace OXGaming.TibiaAPI.Network.ServerPackets
             {
                 TotalTibiaCoins = message.ReadInt32();
                 TransferableTibiaCoins = message.ReadInt32();
+                if (Client.VersionNumber >= 125010109)
+                {
+                    Unknown = message.ReadInt32();
+                }
                 if (Client.VersionNumber >= 12158493)
                 {
                     TournamentCoins = message.ReadInt32();
@@ -38,6 +43,10 @@ namespace OXGaming.TibiaAPI.Network.ServerPackets
             {
                 message.Write(TotalTibiaCoins);
                 message.Write(TransferableTibiaCoins);
+                if (Client.VersionNumber >= 125010109)
+                {
+                    message.Write(Unknown);
+                }
                 if (Client.VersionNumber >= 12158493)
                 {
                     message.Write(TournamentCoins);
